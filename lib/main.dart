d49432b83f1b34af7c1b666d938b9b6f8fc40275
Aghiad-Alzein-78 <aghiad_alzein@yahoo.com>
2021-09-20 18:15:55 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  var questions = [
+    "What's your favourite color?",
+    "What's your favorite animal?",
+  ];
+  // This widget is the root of your application.
+  void answerQuestion() {
+    setState(() {
+      questionIndex = ((questionIndex + 1) % questions.length) ~/ 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              onPressed: () {
+                print("$questionIndex");
+                answerQuestion();
+              },
+              child: Text("Answer 1"),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 2"),
+            ),
+            RaisedButton(
+              onPressed: () => answerQuestion(),
+              child: Text("Answer 3"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
